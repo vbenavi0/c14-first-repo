25647689c8aff9729212105e9777879a455bd609
Victor Benavides <victorvill.0988@gmail.com>
2023-10-04 14:32:45 -0400

    added mutation lesson content

---
+++ 03 Mutation/script.js
@@ -0,0 +1,16 @@
+let firstName = 'Victor';
+let age = 19;
+
+age = 21;
+firstName = 'Daniel';
+console.log(firstName);
+console.log(age);
+
+let message = 'My age is '+age;
+console.log(message);
+console.log(typeof(message));
+
+let result = '10' - 5;
+console.log(result);
+
+let value = true;
